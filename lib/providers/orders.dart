bacf479b892d113e9adb1814ecaf590dbf5eb32b
Aleksandar Velichkovikj <al.velichkovikj@gmail.com>
2022-11-09 05:57:30 +0100

    fix to show most recent orders first

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -51,7 +51,7 @@ class Orders with ChangeNotifier {
               .toList(),
         ));
       });
-      _orders = loadedOrders;
+      _orders = loadedOrders.reversed.toList();
       notifyListeners();
     } catch (error) {
       print(json.decode(error.body));
